9afe18d62f446cddb9bbe41135e8fa284f9874bf
omnia <eng.omniayehia@gmail.com>
2023-04-10 20:42:12 +0100

    manage req&res

--- routes/req.js
+++ routes/req.js
@@ -1,4 +1,3 @@
-
 const router=require('express').Router();
 const conn=require("../db/connection");
 const adminAuth=require("../middleware/admin");
@@ -11,28 +10,24 @@ router.post('/send-request' , (req, res) => {
 
    conn.query("INSERT INTO requests set ?",
    {reader_id:data.reader_id,book_id:data.book_id},(error,result, fields)=>{
+    console.log(error);
     if(error){
         res.statusCode=500;
         res.json({
-
-
           message:"error in req",
-          
       });
       }
       else{
       res.json({
-
         message:"req send successfully",
       } );
     }
     })
-  
     });
    
     //admin retrieve all requests
     router.get("/",adminAuth, function (req, res) {
-      conn.query("select * from  requests",(error,result,fields)=>{
+      conn.query("select requests.id ,users.email, books.Book_Name,requests.status_req  from requests inner join books on requests.book_id=books.id inner join users on requests.reader_id=users.id where status_req = 'pending'",(error,result,fields)=>{
         res.json(result);
   
       });
@@ -42,24 +37,31 @@ router.post('/send-request' , (req, res) => {
    //reader retrieve his requests
     router.get("/:id", function (req, res) {
       const {id} = req.params ;
-      conn.query("select * from requests where ?",{reader_id :id },(error,result,fields)=>{
-        if(error){
-          res.statusCode=500;
+      conn.query("select * from requests INNER JOIN books ON requests.book_id = books.id  WHERE requests.reader_id  = ?",[id],(error,result,fields)=>{
+        if(result.length == 0){
           res.json({
-  
             message:"req not found",
         });
       }
-
+      else{
         res.json(result );
-
-
+      }
       })
-    
-
   });
+ // get on request to one book
+ router.get("/:reader_id/:book_id", function (req, res) {
+  const {reader_id} = req.params ;
+  const {book_id} = req.params ;
+  conn.query(`select * from requests where reader_id =${reader_id} and book_id=${book_id} `,(error,result,fields)=>{
+    if(result.length == 0){
+      res.json({
+        message:"req not found",
+    });
+  }
+  else{
+    res.json(result );
+  }
+  })
+});
 
-
-  
-  
 module.exports = router;
--- routes/response.js
+++ routes/response.js
@@ -1,4 +1,4 @@
- const router=require('express').Router();
+const router=require('express').Router();
  const conn=require("../db/connection");
  const adminAuth=require("../middleware/admin");
 
@@ -7,13 +7,9 @@
   
  router.put("/accept/:id",adminAuth, function (req, res) {
   
-  
   const {id} = req.params ;
- 
-  const data = req.body ;
   
-
-  conn.query("update requests set ? where id = ?",[{status_req:data.status_req},id],(err,result)=>{
+  conn.query("update requests set status_req ='accepted' where id = ?",id,(err,result)=>{
 
 
 if(err)
@@ -39,10 +35,10 @@ router.put("/reject/:id",adminAuth, function (req, res) {
   
   const {id} = req.params ;
  
-  const data = req.body ;
+
   
 
-  conn.query("update requests set ? where id = ?",[{status_req:data.status_req},id],(err,result)=>{
+  conn.query("update requests set status_req ='rejected' where id = ?",id,(err,result)=>{
 
 
 if(err)
@@ -68,27 +64,5 @@ res.json({
 
 
 
-  // accept a request
-/*app.put('/requests/:id/accept', (req, res) => {
-  const requestId = parseInt(req.params.id);
-  const requestIndex = requests.findIndex(request => request.id === requestId);
-
-  if (requestIndex === -1) {
-    return res.status(404).json({ error: 'Request not found.' });
-  }
-
-  const request = requests[requestIndex];
-
-  // check if book is still available
-  const book = books.find(book => book.id === request.bookId);
-  if (!book.available) {
-    return res.status(400).json({ error: 'Book is no longer available for borrowing.' });
-  }
-
-  // update request and book availability
-  request.status = 'accepted';
-  book.available = false;
-
-  res.json(request);
-});*/
+  
   module.exports=router;
